1fa664fbb723d348817d78681d99cce8de8bfec9
frsiam <siam.frs@gmail.com>
2022-02-10 20:28:30 +0600

    copy the html file from previpus version of banking website and create new javascript file

---
+++ JS/banking.js
@@ -0,0 +1,7 @@
+//Deposit
+
+document.getElementById('deposite-button').addEventListener('click',function(){
+    const depositeInput = document.getElementById('deposite-input');
+    const depositeText = depositeInput.value;
+    console.log(depositeText);
+})
