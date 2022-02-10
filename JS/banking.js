d9fe30c3954615d6be15a16d7a766d30ca99a4b7
frsiam <siam.frs@gmail.com>
2022-02-11 01:08:29 +0600

    create another common function for update withdraw and deposite

--- JS/banking.js
+++ JS/banking.js
@@ -1,4 +1,5 @@
 // common function
+
 function getInput(id){
     // deposit new input
     const NewInput = document.getElementById(id);
@@ -9,16 +10,25 @@ function getInput(id){
     return NewAmount;
 }
 
-
-//Deposit
-document.getElementById('deposite-button').addEventListener('click',function(){
-    const NewAmount = getInput('deposite-input')
-    // get current deposite
-    const depositCurrentTotal = document.getElementById('deposit-total');
+function getUpdate(id,NewAmount){
+    const depositCurrentTotal = document.getElementById(id);
     const depositCurrentText = depositCurrentTotal.innerText;
     const depositCurrentAmount = parseFloat(depositCurrentText);
 
     depositCurrentTotal.innerText = NewAmount + depositCurrentAmount;
+}
+
+
+//Deposit
+document.getElementById('deposite-button').addEventListener('click',function(){
+    const NewAmount = getInput('deposite-input')
+    // get and update deposite total
+    /* const depositCurrentTotal = document.getElementById('deposit-total');
+    const depositCurrentText = depositCurrentTotal.innerText;
+    const depositCurrentAmount = parseFloat(depositCurrentText);
+
+    depositCurrentTotal.innerText = NewAmount + depositCurrentAmount; */
+    getUpdate('deposit-total',NewAmount);
 
     //update balance 
     const balanceTotal = document.getElementById('total-balance');
@@ -33,9 +43,11 @@ document.getElementById('withdraw-button').addEventListener('click',function(){
     const withdrawNewAmount = getInput('withdraw-input')
     
     // get current withdraw
-    const withdrawCurrentTotal = document.getElementById('withdraw');
+    /* const withdrawCurrentTotal = document.getElementById('withdraw');
     const withdrawCurrentText = withdrawCurrentTotal.innerText;
-    const withdrawCurrentAmount = parseFloat(withdrawCurrentText);
+    const withdrawCurrentAmount = parseFloat(withdrawCurrentText); */
+
+    getUpdate('withdraw',withdrawNewAmount);
 
     withdrawCurrentTotal.innerText = withdrawCurrentAmount + withdrawNewAmount;
 
