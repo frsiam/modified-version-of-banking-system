75f240227d4ba62b041dc07083da9d1768d56566
frsiam <siam.frs@gmail.com>
2022-02-10 23:19:32 +0600

    convart the innertext into floating number by using parse function and than sum the total

--- JS/banking.js
+++ JS/banking.js
@@ -4,12 +4,14 @@ document.getElementById('deposite-button').addEventListener('click',function(){
     // deposit new input
     const depositNewInput = document.getElementById('deposite-input');
     const depositNewText = depositNewInput.value;
+    const depositNewAmount = parseFloat(depositNewText)
 
     // get current deposite
     const depositCurrentTotal = document.getElementById('deposit-total');
     const depositCurrentText = depositCurrentTotal.innerText;
+    const depositCurrentAmount = parseFloat(depositCurrentText);
 
-    depositCurrentTotal.innerText = depositNewText;
+    depositCurrentTotal.innerText = depositNewAmount + depositCurrentAmount;
     console.log(depositCurrentText);
 
     // clear input field
